e64c990c94d85830ef1316732d83645e9530eb71
satya00089 <satyasubudhi089@gmail.com>
2026-08-28 11:06:27 +0530

    feat: update environment configuration and add Inter font support

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
 import "@xyflow/react/dist/style.css";
 import "./index.css";
 import "./styles/driver-overrides.css";
